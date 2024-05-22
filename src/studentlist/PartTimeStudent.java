530d1b6aaf47d418166ea0a1fd23b348a8ce796b
Jasmeet112 <kau16345@sheridancollege.ca>
2024-05-22 17:33:40 -0400

    Branch

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,14 @@
+package studentlist;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jassk
+ */
+public class PartTimeStudent extends Student {
+    private int x;
+}
